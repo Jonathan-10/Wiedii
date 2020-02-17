f2e661f452d0a2c1cbe3782e052d13ba79d7f67d
Jonathan Ramirez <holamundo@gmail.com>
2020-02-17 11:26:03 -0500

    Solo Login

---
+++ app/src/main/java/com/example/proyecto/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.proyecto
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/com/example/proyecto/login.kt
@@ -0,0 +1,33 @@
+package com.example.proyecto
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.login.*
+
+
+class login : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sesionButton.setOnClickListener {
+
+            findNavController().navigate(R.id.action_login_to_destino1)
+
+        }
+    }
+
+
+}
